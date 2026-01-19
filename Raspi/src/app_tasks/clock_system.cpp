d2b433109c996f88b423451d4c266635bf76a392
Ricky <rikkii.takamoto7589@gmail.com>
2026-01-19 22:22:19 +0900

    一度検証用。 <info> 	おそらくthread関係でエラーが起きている。  On branch clock_system/get_now_time  Changes to be committed: 	modified:   Raspi/inc/app_tasks/clock_system.hpp 	modified:   Raspi/src/app_tasks/clock_system.cpp

--- Raspi/src/app_tasks/clock_system.cpp
+++ Raspi/src/app_tasks/clock_system.cpp
@@ -3,10 +3,9 @@
 
 #include"clock_system.hpp"
 
-void clock_system::get_now_time(time_t *now_time){
-    while (true){
-        std::chrono::system_clock::time_point system_now_time = std::chrono::system_clock::now();
-        now_time = std::chrono::system_clock::to_time_t(system_now_time);
-        return;
-    }    
+void clock_system::get_now_time(std::tm *now_time){
+    while(true){
+        *now_time = *std::localtime(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+    }
+    return;    
 }
