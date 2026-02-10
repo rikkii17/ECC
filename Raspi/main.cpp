d90c85254e6c3099736cba698975d17f0da6a040
Ricky <rikkii.takamoto7589@gmail.com>
2026-02-10 14:33:55 +0900

    システム終了時の動作の安定性を工場

--- Raspi/main.cpp
+++ Raspi/main.cpp
@@ -23,28 +23,36 @@
 int main(){
     std::tm test;
 
-    std::cout <<"システムを立ち上げています"<<std::endl;
-    
-    sf::RenderWindow clockwindow(sf::VideoMode::getDesktopMode(),"fullscreen",sf::State::Fullscreen);
+    std::cout <<"Starting up the system"<<std::endl;
 
     // ハードウェアパッケージの初期化
-    std::cout <<"\t ハードウェアを初期化中"<<std::endl;
+    std::cout <<"\t Initializing hardware"<<std::endl;
     
-    std::cout<<"アプリケーションを立ち上げています"<<std::endl;
+
+    std::cout<<"Launching the application"<<std::endl;
+
+    std::cout<<"\t starting the clock system"<<std::endl;
     std::thread system_clock_thread(clock_system::set_now_time);
 
+    std::cout<<"\t starting the clock GUI window \n \n \n"<<std::endl;
+    sf::RenderWindow clockwindow(sf::VideoMode::getDesktopMode(),"fullscreen",sf::State::Fullscreen);
+
     while(clockwindow.isOpen()){
-        std::tm now_time = clock_system::get_now_time();
+        while(std::optional event = clockwindow.pollEvent()){
+            std::tm now_time = clock_system::get_now_time();
 
-        if(hardwere_config::controller::get_system_is_running() == false) std::cout<<"end"<<std::endl;
-
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape) == true)   hardwere_config::controller::put_system_is_running(false);
-        
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape) == true){
+                clockwindow.close();
+                hardwere_config::controller::put_system_is_running(false);
+            }
+        }
         if(hardwere_config::controller::get_system_is_running() == false)  break;
     }    
 
+    std::cout<<"\n \n \n Shutting down system"<<std::endl;
     //終了操作
     system_clock_thread.join();
+
+    std::cout<<"Bye"<<std::endl;
     return 0;
 }
